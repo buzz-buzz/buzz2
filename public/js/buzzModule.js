61102cf238f8d98e823e41bfd2d311ccf600b831
田杰 <jeff.tian@outlook.com>
2016-11-18 13:33:49 +0800

    Fix profile avatar.

--- public/js/buzzModule.js
+++ public/js/buzzModule.js
@@ -2,6 +2,10 @@ angular.module('buzzModule', ['angularQueryParserModule', 'servicesModule', 'cli
     .run(['$rootScope', 'service', 'clientConfig', function ($rootScope, service, clientConfig) {
         service.get(clientConfig.serviceUrls.sso.profile.load.frontEnd).then(function (result) {
             $rootScope.profile = result;
+
+            if ($rootScope.profile.avatar && $rootScope.profile.avatar.indexOf('upload.bridgeplus.cn') === 0) {
+                $rootScope.profile.avatar += '-minor';
+            }
         });
     }])
     .controller('headerCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
