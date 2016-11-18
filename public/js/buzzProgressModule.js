b66d97847dce6a4e7e7665b1b0b08d105e4ecd54
田杰 <jeff.tian@outlook.com>
2016-11-18 19:06:37 +0800

    Show performance.

--- public/js/buzzProgressModule.js
+++ public/js/buzzProgressModule.js
@@ -1,21 +1,20 @@
 angular.module('buzzProgressModule', ['angularQueryParserModule', 'servicesModule', 'clientConfigModule', 'buzzHeaderModule'])
     .controller('calendarCtrl', ['$scope', function ($scope) {
+        $scope.today = new Date();
         $scope.current = new Date();
+        $scope.performances = [
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null]
+        ];
+
         dateChanged();
 
         $scope.gotoMonth = function (diff) {
             $scope.current.setMonth($scope.current.getMonth() + diff);
             dateChanged();
-        };
-
-        var WeekDaySequence = {
-            Sunday: 0,
-            Monday: 1,
-            Tuesday: 2,
-            Wednesday: 3,
-            Thursday: 4,
-            Friday: 5,
-            Saturday: 6
         };
 
         function dateChanged() {
@@ -39,7 +38,32 @@ angular.module('buzzProgressModule', ['angularQueryParserModule', 'servicesModul
 
                 for (var j = start; j < $scope.weekDays[i].length && day <= days; j++) {
 
-                    $scope.weekDays[i][j] = day++;
+                    $scope.weekDays[i][j] = new Date($scope.current.getFullYear(), $scope.current.getMonth(), day++, 0, 0, 0);
+                }
+            }
+
+            getPerformances();
+        }
+
+        function getPerformance(date) {
+            console.log('today = ', $scope.today, '; date = ', date);
+            if (!date || date >= $scope.today) {
+                console.log('not valid');
+                return '';
+            }
+
+            var a = ['good', 'bad', 'none'];
+
+            let result = a[Math.round(Math.random() * 2)];
+            console.log('result = ', result);
+            return result;
+        }
+
+
+        function getPerformances() {
+            for (var i = 0; i < $scope.weekDays.length; i++) {
+                for (var j = 0; j < $scope.weekDays[i].length; j++) {
+                    $scope.performances[i][j] = getPerformance($scope.weekDays[i][j]);
                 }
             }
         }
