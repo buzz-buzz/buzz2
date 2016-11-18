70daa351b588e050ccb4c24732aa3e19edfe0813
田杰 <jeff.tian@outlook.com>
2016-11-18 15:33:17 +0800

    use layout for my pages.

---
+++ public/js/buzzHeaderModule.js
@@ -0,0 +1,13 @@
+angular.module('buzzHeaderModule', ['angularQueryParserModule', 'servicesModule', 'clientConfigModule'])
+    .run(['$rootScope', 'service', 'clientConfig', function ($rootScope, service, clientConfig) {
+        service.get(clientConfig.serviceUrls.sso.profile.load.frontEnd).then(function (result) {
+            $rootScope.profile = result;
+
+            if ($rootScope.profile.avatar && $rootScope.profile.avatar.indexOf('//upload.bridgeplus.cn') === 0) {
+                $rootScope.profile.avatar += '-minor';
+            }
+        });
+    }])
+    .controller('headerCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    }])
+;
--- public/js/buzzModule.js
+++ public/js/buzzModule.js
@@ -1,15 +1,4 @@
-angular.module('buzzModule', ['angularQueryParserModule', 'servicesModule', 'clientConfigModule'])
-    .run(['$rootScope', 'service', 'clientConfig', function ($rootScope, service, clientConfig) {
-        service.get(clientConfig.serviceUrls.sso.profile.load.frontEnd).then(function (result) {
-            $rootScope.profile = result;
-
-            if ($rootScope.profile.avatar && $rootScope.profile.avatar.indexOf('//upload.bridgeplus.cn') === 0) {
-                $rootScope.profile.avatar += '-minor';
-            }
-        });
-    }])
-    .controller('headerCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
-    }])
+angular.module('buzzModule', ['angularQueryParserModule', 'servicesModule', 'clientConfigModule', 'buzzHeaderModule'])
     .controller('VideoPlayerCtrl', ['$scope', '$sce', function ($scope, $sce) {
         $scope.queryString = location.search;
         $scope.$sce = $sce;
