c0fd9bc844ed5eec34454a74b9ba12dac38e41bc
田杰 <jeff.tian@outlook.com>
2016-11-24 10:49:45 +0800

    improve

--- public/js/full-page.js
+++ public/js/full-page.js
@@ -4,9 +4,11 @@
     document.onscroll = function (event) {
         for (var i = 0; i < pages.length; i++) {
             if (pages[i].offsetTop - document.body.scrollTop < document.getElementById('buzz-header').offsetHeight) {
-                pages[i].nextSibling.className += ' ' + 'with-header';
+                if (pages[i].nextSibling.className.indexOf('with-header') < 0) {
+                    pages[i].nextSibling.className += ' ' + 'with-header';
+                }
             } else {
-                pages[i].nextSibling.className = pages[i].nextSibling.className.replace('with-header', '');
+                pages[i].nextSibling.className = pages[i].nextSibling.className.replace(/ *with-header */, '');
             }
         }
     };
