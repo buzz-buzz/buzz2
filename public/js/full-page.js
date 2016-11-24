c7586fde4d4c069e7c42e98bab1d551343c5e656
田杰 <jeff.tian@outlook.com>
2016-11-24 10:35:35 +0800

    Fix bug.

--- public/js/full-page.js
+++ public/js/full-page.js
@@ -1,7 +1,9 @@
 (function () {
+    var pages = document.getElementsByClassName('page-pos');
+
     document.onscroll = function (event) {
         for (var i = 0; i < pages.length; i++) {
-            if (pages[i].offsetTop - document.body.scrollTop < 40) {
+            if (pages[i].offsetTop - document.body.scrollTop < document.getElementById('buzz-header').offsetHeight) {
                 pages[i].nextSibling.className += ' ' + 'with-header';
             } else {
                 pages[i].nextSibling.className = pages[i].nextSibling.className.replace('with-header', '');
