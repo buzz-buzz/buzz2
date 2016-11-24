bf67da7aa5e2f84ebb7434add6ce5968c4e3732f
田杰 <jeff.tian@outlook.com>
2016-11-24 11:28:15 +0800

    Show page2 header on first screen.

--- public/js/buzzModule.js
+++ public/js/buzzModule.js
@@ -20,9 +20,9 @@ angular.module('buzzModule', ['angularQueryParserModule', 'servicesModule', 'cli
         $scope.currentLevel = query.level;
     }])
     .controller('page2ParentCtrl', ['$scope', function ($scope) {
-        $scope.tabularIndex = 1;
+        $scope.$root.tabularIndex = 1;
     }])
-    .controller('quizCtrl', ['$scope', 'queryParser', '$sce',function ($scope, queryParser, $sce) {
+    .controller('quizCtrl', ['$scope', 'queryParser', '$sce', function ($scope, queryParser, $sce) {
         var query = queryParser.parse();
         $scope.$sce = $sce;
         $scope.quizURL = 'http://content.bridgeplus.cn/buzz-quiz/' + query.date + '-' + query.level + '/index.html';
--- public/js/full-page.js
+++ public/js/full-page.js
@@ -1,7 +1,24 @@
 (function () {
     var pages = document.getElementsByClassName('page-pos');
 
-    document.onscroll = function (event) {
+    var page2Header = document.getElementById('page-2-header');
+    var page2 = document.getElementsByClassName('page-pos')[1].nextSibling;
+    var originalPos = page2Header.parentNode;
+    var moved = false;
+
+    function adjustPage2Header() {
+        if (document.body.scrollTop > 40) {
+            if (!moved) {
+                page2.insertBefore(page2Header, page2.firstChild);
+                moved = true;
+            }
+        } else {
+            originalPos.appendChild(page2Header);
+            moved = false;
+        }
+    }
+
+    function handleClassName() {
         for (var i = 0; i < pages.length; i++) {
             if (pages[i].offsetTop - document.body.scrollTop < document.getElementById('buzz-header').offsetHeight) {
                 if (pages[i].nextSibling.className.indexOf('with-header') < 0) {
@@ -11,5 +28,12 @@
                 pages[i].nextSibling.className = pages[i].nextSibling.className.replace(/ *with-header */, '');
             }
         }
-    };
+    }
+
+    function handlePageScroll(event) {
+        adjustPage2Header();
+        handleClassName();
+    }
+
+    document.onscroll = handlePageScroll;
 })();
