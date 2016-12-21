69405c776287bdd8d3aa1f4b985cbcd8cc8bb814
Henry Huang <happybridge14@163.com>
2016-12-21 13:19:21 +0800

    UPdate course history

--- public/js/buzzHistoryModule.js
+++ public/js/buzzHistoryModule.js
@@ -2,7 +2,9 @@ angular.module('buzzHistoryModule', ['angularQueryParserModule'])
     .controller('historyCtrl', ['$scope', '$http', 'queryParser', '$timeout', function ($scope, $http, queryParser, $timeout) {
         $http.get('/api/history-courses').then(function (result) {
             var level = queryParser.get('level') || 'B';
-            $scope.courseList = result.data.byLevel[level];
+            $scope.courseList = result.data.byLevel[level].sort(function(a, b) {
+                return (new Date(b)).valueOf() >= (new Date(a)).valueOf();
+            });
             console.log($scope.courseList);
             for (var i = 0; i < $scope.courseList.length; i++) {
                 var smilJson = '/resource/smil/' + $scope.courseList[i] + '-' + level + '.json';
@@ -12,13 +14,17 @@ angular.module('buzzHistoryModule', ['angularQueryParserModule'])
                 (function (i) {
                     $http.get(smilJson).then(function (result) {
                         var smil = result.data;
-
-                        $scope.courseInfo[$scope.courseList[i]] = {
-                            title: smil.title
+                        var date = $scope.courseList[i];
+                        $scope.courseInfo[date] = {
+                            title: smil.title,
+                            date: date
                         };
                     });
                 })(i);
             }
+            $scope.aLikeClick = function(href) {
+                window.location.href = href;
+            };
         });
     }])
 ;
