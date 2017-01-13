28959c9d5b29d23d56322c1bbcfd84db71945fb1
叽歪 <jeff.tian@outlook.com>
2017-01-13 10:54:17 +0800

    Optimize static resources.

--- routes/static.js
+++ routes/static.js
@@ -2,14 +2,18 @@ const serveStatic = require('koa-static');
 const koaMount = require('koa-mount');
 const proxy = require('../service-proxy/proxy');
 
+let staticSetting = {
+    etag: true,
+    maxage: 1000 * 3600 * 24 * 30,
+    gzip: true
+};
+
 module.exports = function (app) {
-    app.use(serveStatic('public'));
-    app.use(koaMount('/public/', serveStatic('public')));
-    app.use(koaMount('/mock/', serveStatic('mock')));
-    app.use(koaMount('/node_modules', serveStatic('node_modules')));
-    if (process.env.NODE_ENV === 'prd') {
-        app.use(koaMount('/resources', serveStatic('resources')));
-    } else {
+    app.use(serveStatic('public', staticSetting));
+    app.use(koaMount('/public/', serveStatic('public', staticSetting)));
+    app.use(koaMount('/mock/', serveStatic('mock', staticSetting)));
+    app.use(koaMount('/node_modules', serveStatic('node_modules', staticSetting)));
+    if (process.env.NODE_ENV !== 'prd') {
         app.use(koaMount('/resources', function *(next) {
             this.body = yield proxy({
                 host: 'www.buzzbuzzenglish.com',
