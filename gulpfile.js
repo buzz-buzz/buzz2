f2a97db02f6623c8d5a3b47f76490b9f9d02af36
叽歪 <jeff.tian@outlook.com>
2017-01-13 11:49:48 +0800

    Add gulp task.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let gulp = require('gulp');
+let runSequence = require('run-sequence');
+let uglify = require('gulp-uglify');
+let uglifyCss = require('gulp-minify-css');
+let rename = require('gulp-rename');
+
+gulp.task('uglify-js', function (done) {
+    return gulp.src('public/js/**/*.js')
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.basename += '.min';
+        }))
+        .pipe(gulp.dest('public/js'))
+        ;
+});
+
+gulp.task('uglify-css', function (done) {
+    return gulp.src('public/css/**/*.css')
+        .pipe(uglifyCss())
+        .pipe(rename(function (path) {
+            path.basename += '.min';
+        }))
+        .pipe(gulp.dest('public/css'))
+        ;
+});
+
+gulp.task('default', ['uglify-js', 'uglify-css']);
+
+gulp.task('release', ['uglify-js', 'uglify-css']);
