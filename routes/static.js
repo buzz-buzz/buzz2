66defd9c9251251a85bd4e921e455258e7c20182
Henry Huang <happybridge14@163.com>
2017-01-17 15:30:08 +0800

    Fix koa-static issue

--- routes/static.js
+++ routes/static.js
@@ -1,18 +1,24 @@
+'use strict';
+
 const serveStatic = require('koa-static');
 const koaMount = require('koa-mount');
 const proxy = require('../service-proxy/proxy');
 
-let staticSetting = {
+const staticSetting = {
     etag: true,
     maxage: 1000 * 3600 * 24 * 30,
     gzip: true
 };
+let getStaticSetting = function() {
+    return Object.assign({}, staticSetting);
+};
+
 
 module.exports = function (app) {
-    app.use(serveStatic('public', staticSetting));
-    app.use(koaMount('/public/', serveStatic('public', staticSetting)));
-    app.use(koaMount('/mock/', serveStatic('mock', staticSetting)));
-    app.use(koaMount('/node_modules', serveStatic('node_modules', staticSetting)));
+    app.use(serveStatic('public', getStaticSetting()));
+    app.use(koaMount('/public', serveStatic('public', getStaticSetting())));
+    app.use(koaMount('/mock', serveStatic('mock', getStaticSetting())));
+    app.use(koaMount('/node_modules', serveStatic('node_modules', getStaticSetting())));
     if (process.env.NODE_ENV !== 'prd') {
         app.use(koaMount('/resources', function *(next) {
             this.body = yield proxy({
