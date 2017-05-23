eace3ca0d185f652570a6a3a17002262c28b9687
Jeff Tian <jefftian@hcdlearning.com>
2017-05-23 16:16:47 +0800

    Refactor weekly quiz.

--- public/js/buzzHeaderModule.js
+++ public/js/buzzHeaderModule.js
@@ -114,4 +114,14 @@ angular.module('buzzHeaderModule', ['angularQueryParserModule', 'servicesModule'
             }
         };
     }])
-    ;
+    // .filter('percentage', ['$window', function ($window) {
+    //     return function (input, decimals, suffix) {
+    //         decimals = angular.isNumber(decimals) ? decimals : 3;
+    //         suffix = suffix || '%';
+    //         if ($window.isNaN(input)) {
+    //             return '';
+    //         }
+    //         return Math.round(input * Math.pow(10, decimals + 2)) / Math.pow(10, decimals) + suffix
+    //     };
+    // }]);
+;
