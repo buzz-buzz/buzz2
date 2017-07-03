f326833efc05406227726cee1f83a217f472b39c
hank <13061710755@sina.cn>
2017-07-03 16:53:06 +0800

    update surveyModule.

--- public/js/components/survey/buzzSurveyModule.js
+++ public/js/components/survey/buzzSurveyModule.js
@@ -1,10 +1,11 @@
 angular.module('buzzSurveyModule', ['angularQueryParserModule', 'servicesModule', 'clientConfigModule', 'trackingModule', 'serviceCacheModule'])
     .controller("surveyCtrl", ['$scope', '$rootScope', '$q', 'api', 'clientConfig', function ($scope, $rootScope, $q, api, clientConfig) {
         api.get(clientConfig.serviceUrls.buzz.survey.get.frontEnd).then(function (result) {
-            $scope.surveyUrls = '/survey?url= '+ encodeURIComponent(result.data.survey_url);
+            $scope.surveyUrls = result.data.survey_url;
             if ($scope.surveyUrls == null) {
                 $scope.survey = false;
             } else {
+                $scope.surveyUrls = '/survey?url= '+ encodeURIComponent(result.data.survey_url);
                 $scope.survey = true;
             }
         });
