1941911c5cc09e97640a072c61032d8e7a13597a
Jeff Tian <jefftian@hcdlearning.com>
2017-07-05 17:31:52 +0800

    refine survey layout

--- routes/more/sharable.js
+++ routes/more/sharable.js
@@ -8,21 +8,21 @@ const saas = require('../../bll/saas');
 
 module.exports = function (app, router, render) {
     router
-        .get('/s/player', saas.checkSaasReferer, function*() {
+        .get('/s/player', saas.checkSaasReferer, function* () {
             if (this.state.userAgent.isMobile && !this.state.userAgent.isTablet) {
-                this.body = yield render.call(this, '/m/player', {config: config});
+                this.body = yield render.call(this, '/m/player', { config: config });
             } else {
-                this.body = yield render.call(this, 'my/player', {config: config});
+                this.body = yield render.call(this, 'my/player', { config: config });
             }
         })
-        .get('/s/ad/:trk_tag?', saas.checkSaasReferer, function*() {
+        .get('/s/ad/:trk_tag?', saas.checkSaasReferer, function* () {
             if (!this.query.trk_tag) {
                 this.redirect(saas.generateUrl(this, `/s/ad?trk_tag=${this.params.trk_tag}`));
             } else {
-                this.body = yield render.call(this, 'ad', {config: config, trk_tag: this.query.trk_tag});
+                this.body = yield render.call(this, 'ad', { config: config, trk_tag: this.query.trk_tag });
             }
         })
-        .get('/agreement', saas.checkSaasReferer, function*() {
+        .get('/agreement', saas.checkSaasReferer, function* () {
             let view = '/agreement';
 
             if (this.state.userAgent.isMobile && !this.state.userAgent.isTablet) {
@@ -32,9 +32,9 @@ module.exports = function (app, router, render) {
             this.body = yield render.call(this, view, {
                 config: config,
                 base: saas.getBaseFor(this, '/'),
-                title: 'Buzzbuzz用户协议',
+                title: 'Buzzbuzz 用户协议',
                 backUrl: 'javascript:location.href="/sign-up"'
             });
         })
-    ;
+        ;
 };
