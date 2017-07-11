d4bdb4bd63572a61432e1680b8c3d12dff899db5
Jeff Tian <jefftian@hcdlearning.com>
2017-07-11 12:59:13 +0800

    Add root domain

--- helpers/cookie.js
+++ helpers/cookie.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const config = require('../config');
+
 let clearCookieOption = {
     expires: new Date(1970, 1, 1),
     path: '/',
@@ -10,6 +12,10 @@ let sessionCookieOption = {
     path: '/',
     httpOnly: true
 };
+
+if (process.env.NODE_ENV === 'prd') {
+    sessionCookieOption.domain = config.rootDomain;
+}
 
 let o = {
     setToken: function (token) {
