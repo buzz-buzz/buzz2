b8945b73cd4766d7191257bb0ddf18e5c9ac2484
Jeff Tian <jefftian@hcdlearning.com>
2017-08-16 10:57:59 +0800

    Don't allow browser to cache homepage

--- routes/start/homepage.js
+++ routes/start/homepage.js
@@ -7,6 +7,8 @@ const courseList = require('../common/course-list');
 
 module.exports = function (app, router, render) {
     router.get('/', saas.checkSaasReferer, membership.setHcdUserIfSignedIn, function* home(next) {
+        this.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+
         if (this.state.hcd_user) {
             this.redirect(saas.generateUrl(this, '/my/today'));
         } else {
