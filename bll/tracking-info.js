7c86bda048e1dede01dfadbb39ce91d8ff08d704
hank <13061710755@sina.cn>
2017-08-17 10:26:56 +0800

    err in tracking-info.js  .

--- bll/tracking-info.js
+++ bll/tracking-info.js
@@ -1,17 +1,20 @@
 'use strict';
 
-const {
-    URL,
-    URLSearchParams
-} = require('url');
+
+
+const URL = require('url');
 
 module.exports = {
     checkReferer: function* (next) {
         if (this.req.headers.referer && (this.req.headers.referer.indexOf('/trk_tag') >= 0 || this.req.headers.referer.indexOf('channel') >= 0) &&
             (this.originalUrl.indexOf('trk_tag') < 0 && this.originalUrl.indexOf('channel') < 0)) {
             let sep = this.url.indexOf('?') < 0 ? '?' : '&';
-            let parsed = new URL(this.req.headers.referer);
-            return this.redirect(`${this.url}${sep}trk_tag=${parsed.searchParams.get('trk_tag')}&channel=${parsed.searchParams.get('channel')}`);
+            console.log('=====');
+            console.dir(URL);
+            let parsed = URL.parse(this.req.headers.referer, true).query;
+            console.log(parsed);
+
+            return this.redirect(`${this.url}${sep}trk_tag=${parsed.trk_tag || ''}&channel=${parsed.channel || ''}`);
         }
 
         yield next;
