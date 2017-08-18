0fe929517e88c21d012eb376ba2d7bd194e6bd0c
LIANJINLEI <1175968487@qq.com>
2017-08-18 17:00:41 +0800

    sign-up add prompt message

--- public/js/formModule.js
+++ public/js/formModule.js
@@ -1,15 +1,15 @@
 angular.module('formModule', ['clientConfigModule', 'servicesModule', 'errorParserModule', 'trackingModule'])
-    .config(['$sceProvider', function ($sceProvider) {
+    .config(['$sceProvider', function($sceProvider) {
         $sceProvider.enabled(false);
     }])
-    .directive('captcha', ['service', 'clientConfig', 'tracking', '$sce', '$http', function (service, clientConfig, tracking, $sce, $http) {
+    .directive('captcha', ['service', 'clientConfig', 'tracking', '$sce', '$http', function(service, clientConfig, tracking, $sce, $http) {
         var captchaServiceOrigin = clientConfig.captcha.origin;
 
         return {
             restrict: 'A',
             require: 'ngModel',
-            template: '<img ng-src="{{captchaImageUrl}}" ng-click="refreshCaptcha()" style="max-height: 100%;">',
-            link: function ($scope, $element, attrs, ngModel) {
+            template: '<img ng-src="{{captchaImageUrl}}" ng-click="refreshCaptcha()" alt="点我刷新" style="max-height: 100%;">',
+            link: function($scope, $element, attrs, ngModel) {
                 function errorHandler(res) {
                     console.error(res);
                     tracking.sendX('sign-up.changeIdentifyCode.error')
@@ -27,9 +27,8 @@ angular.module('formModule', ['clientConfigModule', 'servicesModule', 'errorPars
                     tracking.sendX('sign-up.changeIdentifyCode');
                     var url = captchaServiceOrigin + '/captcha/generator/p?appid=bplus';
                     $http.jsonp(url)
-                        .then(function (result) {
+                        .then(function(result) {
                             result = result.data;
-
                             if (result.isSuccess) {
                                 result = result.result;
                                 $scope.captchaImageUrl = captchaServiceOrigin + result.url;
@@ -43,7 +42,7 @@ angular.module('formModule', ['clientConfigModule', 'servicesModule', 'errorPars
                             }
                         })
                         .catch(errorHandler)
-                        .finally(function () {
+                        .finally(function() {
                             refreshing = false;
                             tracking.sendX('sign-up.changeIdentifyCode.done')
                         });
@@ -55,17 +54,17 @@ angular.module('formModule', ['clientConfigModule', 'servicesModule', 'errorPars
             }
         };
     }])
-    .directive('resendVerification', ['service', 'serviceErrorParser', '$rootScope', '$timeout', 'clientConfig', 'tracking', function (service, serviceErrorParser, $rootScope, $timeout, clientConfig, tracking) {
+    .directive('resendVerification', ['service', 'serviceErrorParser', '$rootScope', '$timeout', 'clientConfig', 'tracking', function(service, serviceErrorParser, $rootScope, $timeout, clientConfig, tracking) {
         return {
             template: '<button class="yellow button verification" type="button" ng-click="sendVerificationCode()" ng-class="{\'loading\': sendingVerificationCode}" ng-disabled="!allowSendingVerification()" style="font-size: small;">{{verificationCodeButtonText}}</button>',
             replace: true,
-            link: function ($scope, $element, attrs) {
+            link: function($scope, $element, attrs) {
                 var countDownInterval = 60;
                 var countDown = countDownInterval;
 
                 $scope.changeMobileData = {};
 
-                $scope.allowSendingVerification = function () {
+                $scope.allowSendingVerification = function() {
                     return $scope.allowGetCode && /\d+/.test($scope.signUpData.mobile) && $scope.signUpData.captcha;
                 };
 
@@ -90,7 +89,7 @@ angular.module('formModule', ['clientConfigModule', 'servicesModule', 'errorPars
                     refreshButtonText();
 
                     if (countDown > 0) {
-                        $timeout(function () {
+                        $timeout(function() {
                             countDown--;
                             pollUpdateButtonText(roundEndCallback);
                         }, 1000);
@@ -108,21 +107,21 @@ angular.module('formModule', ['clientConfigModule', 'servicesModule', 'errorPars
 
                 $scope.sendingVerificationCode = false;
                 $scope.verificationButtonClicked = false;
-                $scope.sendVerificationCode = function () {
+                $scope.sendVerificationCode = function() {
                     tracking.sendX('sign-up.identifyPhone');
-                    service.executePromiseAvoidDuplicate($scope, 'sendingVerificationCode', function () {
+                    service.executePromiseAvoidDuplicate($scope, 'sendingVerificationCode', function() {
                             return service.put(clientConfig.serviceUrls.sms.sendWithCaptcha.frontEnd, {
                                 captchaId: $scope.signUpData.captchaId,
                                 captcha: $scope.signUpData.captcha,
                                 mobile: $scope.signUpData.mobile
                             });
                         })
-                        .then(function (result) {
+                        .then(function(result) {
                             $scope.verificationButtonClicked = true;
                             $scope.successMessage = $rootScope.message = '短信验证码已发送,请注意查收';
 
-                            pollUpdateButtonText(function () {
-                                $scope.refreshCaptcha(function () {
+                            pollUpdateButtonText(function() {
+                                $scope.refreshCaptcha(function() {
                                     $scope.changeMobileData.captcha = '';
                                 });
                             });
@@ -130,7 +129,7 @@ angular.module('formModule', ['clientConfigModule', 'servicesModule', 'errorPars
                             $scope.errorMessage = $rootScope.errorMessage = null;
                             tracking.sendX('sign-up.identifyPhone.done');
                         })
-                        .then(null, function (reason) {
+                        .then(null, function(reason) {
                             $scope.refreshCaptcha();
                             $scope.signUpData.captcha = '';
                             $scope.errorMessage = serviceErrorParser.getErrorMessage(reason);
